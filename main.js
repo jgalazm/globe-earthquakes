d27bb0091ff2246ef99adb3c680ec8606122d21f
Jose Galaz <jdgalaz@uc.cl>
2018-05-25 02:00:41 -0400

    arbol de pascua

--- main.js
+++ main.js
@@ -3,51 +3,38 @@ var czml = [
         "id": "document",
         "name": "CZML Point - Time Dynamic",
         "version": "1.0"
-    },
-    {
-        "id": "point",
-        "availability": "2012-08-04T16:00:00Z/2012-08-04T16:05:00Z",
-        "position": {
-            "cartographicDegrees": [
-                "2012-08-04T16:00:00Z", -70, 20, 150000,
-                "2012-08-04T16:00:40Z", -70, 20, 150000
-            ]
-        },
-        "point": {
-            "color": {
-                "rgba": ["2012-08-04T16:00:00Z", 255, 255, 255, 128,
-                "2012-08-04T16:00:20Z", 255, 0, 0, 255,
-                "2013-08-04T16:00:20Z", 255, 0, 0, 255]
-            },
-            "outlineColor": {
-                "rgba": [255, 0, 0, 128]
-            },
-            "outlineWidth": 3,
-            "pixelSize": 15
-        }
     }
 ];
 
 
 var viewer = new Cesium.Viewer('cesiumContainer', {
-    shouldAnimate: true
+    shouldAnimate: true,
+    imageryProvider: new Cesium.createTileMapServiceImageryProvider({
+        url: '/node_modules/cesium/Build/Cesium/Assets/Textures/NaturalEarthII'
+    }),
 });
 
 var baseQueryString = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"
 var startTime = "&starttime=1990-01-01"
 var endTime = "&endtime=2116-12-31"
-var minMagnitudeString = "&minmagnitude=7.0"
+var minMagnitudeString = "&minmagnitude=6.0"
 var productType = "&producttype=moment-tensor"
 var qString = baseQueryString + startTime + endTime + minMagnitudeString
 
 $.getJSON(qString, function(data){
     data.features.forEach(function(feature, index){
         let date = new Date(feature.properties.time);
+        let dateBlanco = new Date(date.getTime());
+        dateBlanco.setMonth(dateBlanco.getMonth()+1);
+
+        let dateEnd = new Date(date.getTime());
+        dateEnd.setMonth(dateEnd.getMonth()+10);
         let coordinates = feature.geometry.coordinates;
-        console.log(date.toISOString());
+
+            
         let point = {
             "id": "point"+index,
-            "availability": date.toISOString()+"/2019-08-04T16:05:00Z",
+            "availability": date.toISOString()+"/"+dateEnd.toISOString(),
             "position": {
                 "cartographicDegrees": [
                     date.toISOString(), coordinates[0], coordinates[1] , 150000,
@@ -56,13 +43,15 @@ $.getJSON(qString, function(data){
             },
             "point": {
                 "color": {
-                    "rgba": [ 255, 255, 255, 128]
+                    "rgba": [ date.toISOString(), 255, 255, 255, 255,
+                        dateBlanco.toISOString(), 128, 128, 0, 255,
+                        "2019-08-04T16:00:40Z", 128, 128, 0, 255]
                 },
                 "outlineColor": {
-                    "rgba": [255, 0, 0, 128]
+                    "rgba": [255, 255, 255, 255]
                 },
-                "outlineWidth": 3,
-                "pixelSize": 15
+                "outlineWidth": 1,
+                "pixelSize": 10
             }
         }
 
